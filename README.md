9de0599f20f5478e87c44842d8ba8dfe0cd1f4e2
Marcus Lian Hanestad <marlhan@proton.me>
2024-11-05 15:51:47 +0100

    more features and docs

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 SIMD accelerated image swizzling routines.
 
 ## Performance
+
+#### RGBA to BGRA
+
+```
+test tests::bench_serial_rgba_to_bgra     ... bench:  16,276,480.60 ns/iter (+/- 7,499,036.91)
+test tests::bench_vectorized_rgba_to_bgra ... bench:   5,451,023.30 ns/iter (+/- 584,312.97)
+```
+
+On average the verctorized version is 3 times faster than the non vectorized one.
--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,16 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+//! SIMD accelerated image swizzling routines
+
+// TODO: Do conversion depending on system endianess
+
 #![feature(portable_simd)]
 #![feature(test)]
 
 extern crate test;
 
-use std::simd::{simd_swizzle, u8x64};
+use std::simd::{self, simd_swizzle, u8x64};
 
 const VECTOR_WIDTH: usize = 16;
 
@@ -53,10 +57,59 @@ macro_rules! idx_order {
     }
 }
 
+#[rustfmt::skip]
+macro_rules! xxx0_to_xxxx_mask {
+    () => {
+        simd::Mask::from_array([
+            true, true, true, false,
+            true, true, true, false,
+            true, true, true, false,
+            true, true, true, false,
+
+            true, true, true, false,
+            true, true, true, false,
+            true, true, true, false,
+            true, true, true, false,
+
+            true, true, true, false,
+            true, true, true, false,
+            true, true, true, false,
+            true, true, true, false,
+
+            true, true, true, false,
+            true, true, true, false,
+            true, true, true, false,
+            true, true, true, false,
+        ])
+    }
+}
+
 const RGBA_TO_BGRA_SWIZZLE_IDXS: [usize; VECTOR_WIDTH * 4] = idx_order!(2, 1, 0, 3);
 const BGRA_TO_RGBA_SWIZZLE_IDXS: [usize; VECTOR_WIDTH * 4] = idx_order!(2, 1, 0, 3);
+#[rustfmt::skip]
+const XXX0_TO_XXXX_OR: u8x64 = u8x64::from_array([
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
 
-macro_rules! swizzle_4_wide_inline {
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+    0u8, 0u8, 0u8, 255u8,
+]);
+
+macro_rules! swizzle_4_wide_inplace {
     ($src:expr, $idxs:expr) => {
         assert!($src.len() % 4 == 0);
 
@@ -112,17 +165,79 @@ macro_rules! swizzle_4_wide {
     };
 }
 
-#[inline(always)]
-fn serial_swizzle_4_wide_inline(src: &mut [u8], idxs: &[usize; 4]) {
+macro_rules! apply_mask_4_wide_inplace {
+    ($src:expr, $mask:expr, $or:expr) => {
+        assert!($src.len() % 4 == 0);
+
+        let n_pixels = $src.len() / 4;
+
+        let mut i = 0;
+        while i + VECTOR_WIDTH < n_pixels {
+            u8x64::load_select(&$src[i * 4..(i + VECTOR_WIDTH) * 4], $mask, XXX0_TO_XXXX_OR)
+                .copy_to_slice(&mut $src[i * 4..(i + VECTOR_WIDTH) * 4]);
+            i += VECTOR_WIDTH;
+        }
+
+        for j in i..n_pixels {
+            $src[j * 4 + 3] = 255;
+        }
+    };
+}
+
+macro_rules! apply_mask_4_wide {
+    ($src:expr, $dst:expr, $mask:expr, $or:expr) => {
+        assert!($src.len() % 4 == 0 && $src.len() == $dst.len());
+
+        let n_pixels = $src.len() / 4;
+
+        let mut i = 0;
+        while i + VECTOR_WIDTH < n_pixels {
+            u8x64::load_select(&$src[i * 4..(i + VECTOR_WIDTH) * 4], $mask, XXX0_TO_XXXX_OR)
+                .copy_to_slice(&mut $dst[i * 4..(i + VECTOR_WIDTH) * 4]);
+            i += VECTOR_WIDTH;
+        }
+
+        for j in i..n_pixels {
+            $dst[j * 4 + 0] = $src[j * 4 + 0];
+            $dst[j * 4 + 1] = $src[j * 4 + 1];
+            $dst[j * 4 + 2] = $src[j * 4 + 2];
+            $dst[j * 4 + 3] = 255;
+        }
+    };
+}
+
+macro_rules! apply_x_mask_and_swizzle_4_wide {
+    ($src:expr, $dst:expr, $or:expr, $idxs:expr) => {
+        assert!($src.len() % 4 == 0 && $src.len() == $dst.len());
+
+        let n_pixels = $src.len() / 4;
+        let mask = xxx0_to_xxxx_mask!();
+
+        let mut i = 0;
+        while i + VECTOR_WIDTH < n_pixels {
+            simd_swizzle!(
+                u8x64::load_select(&$src[i * 4..(i + VECTOR_WIDTH) * 4], mask, XXX0_TO_XXXX_OR),
+                $idxs
+            )
+            .copy_to_slice(&mut $dst[i * 4..(i + VECTOR_WIDTH) * 4]);
+            i += VECTOR_WIDTH;
+        }
+
+        for j in i..n_pixels {
+            $dst[j * 4 + $idxs[0]] = $src[j * 4 + 0];
+            $dst[j * 4 + $idxs[1]] = $src[j * 4 + 1];
+            $dst[j * 4 + $idxs[2]] = $src[j * 4 + 2];
+            $dst[j * 4 + $idxs[3]] = 255;
+        }
+    };
+}
+
+#[allow(dead_code)]
+fn serial_swizzle_4_wide_inplace(src: &mut [u8], idxs: &[usize; 4]) {
     assert!(src.len() % 4 == 0);
 
     for i in (0..src.len()).step_by(4) {
-        let (a, b, c, d) = (
-            src[i + 0],
-            src[i + 1],
-            src[i + 2],
-            src[i + 3],
-        );
+        let (a, b, c, d) = (src[i], src[i + 1], src[i + 2], src[i + 3]);
         src[i + idxs[0]] = a;
         src[i + idxs[1]] = b;
         src[i + idxs[2]] = c;
@@ -130,33 +245,205 @@ fn serial_swizzle_4_wide_inline(src: &mut [u8], idxs: &[usize; 4]) {
     }
 }
 
-#[inline(always)]
+#[allow(dead_code)]
 fn serial_swizzle_4_wide(src: &[u8], dst: &mut [u8], idxs: &[usize; 4]) {
     assert!(src.len() % 4 == 0);
 
     for i in (0..src.len()).step_by(4) {
-        dst[i + idxs[0]] = src[i + 0];
+        dst[i + idxs[0]] = src[i];
         dst[i + idxs[1]] = src[i + 1];
         dst[i + idxs[2]] = src[i + 2];
         dst[i + idxs[3]] = src[i + 3];
     }
 }
 
-pub fn rgba_to_bgra_inline(src: &mut [u8]) {
-    swizzle_4_wide_inline!(src, RGBA_TO_BGRA_SWIZZLE_IDXS);
+/// Convert RGBA data to BGRA while overwriting the old RGBA data in `src`.
+///
+/// ```rust
+/// use image_swizzle::rgba_to_bgra_inplace;
+/// let mut rgba = [1, 2, 3, 255];
+/// rgba_to_bgra_inplace(&mut rgba);
+/// assert_eq!(rgba, [3, 2, 1, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4.
+pub fn rgba_to_bgra_inplace(src: &mut [u8]) {
+    swizzle_4_wide_inplace!(src, RGBA_TO_BGRA_SWIZZLE_IDXS);
 }
 
+/// Convert RGBA data to BGRA and store the result to `dst`.
+///
+/// ```rust
+/// use image_swizzle::rgba_to_bgra;
+/// let rgba = [1, 2, 3, 255];
+/// let mut bgra = [0; 4];
+/// rgba_to_bgra(&rgba, &mut bgra);
+/// assert_eq!(bgra, [3, 2, 1, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4 or if `dst.len` is not equal to `src.len`.
 pub fn rgba_to_bgra(src: &[u8], dst: &mut [u8]) {
     swizzle_4_wide!(src, dst, RGBA_TO_BGRA_SWIZZLE_IDXS);
 }
 
-pub fn bgra_to_rgba_inline(src: &mut [u8]) {
-    swizzle_4_wide_inline!(src, BGRA_TO_RGBA_SWIZZLE_IDXS);
+/// Convert BGRA data to RGBA while overwriting the old BGRA data in `src`.
+///
+/// ```rust
+/// use image_swizzle::bgra_to_rgba_inplace;
+/// let mut bgra = [3, 2, 1, 255];
+/// bgra_to_rgba_inplace(&mut bgra);
+/// assert_eq!(bgra, [1, 2, 3, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4.
+pub fn bgra_to_rgba_inplace(src: &mut [u8]) {
+    swizzle_4_wide_inplace!(src, BGRA_TO_RGBA_SWIZZLE_IDXS);
 }
 
+/// Convert BGRA data to RGBA and store the result to `dst`.
+///
+/// ```rust
+/// use image_swizzle::bgra_to_rgba;
+/// let bgra = [3, 2, 1, 255];
+/// let mut rgba = [0; 4];
+/// bgra_to_rgba(&bgra, &mut rgba);
+/// assert_eq!(rgba, [1, 2, 3, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4 or if `dst.len` is not equal to `src.len`.
 pub fn bgra_to_rgba(src: &[u8], dst: &mut [u8]) {
     swizzle_4_wide!(src, dst, BGRA_TO_RGBA_SWIZZLE_IDXS);
 }
+
+/// Convert RGB0 data to RGBX while overwriting the old RGB0 data in `src`.
+///
+/// ```rust
+/// use image_swizzle::rgb0_to_rgbx_inplace;
+/// let mut rgb0 = [1, 2, 3, 0];
+/// rgb0_to_rgbx_inplace(&mut rgb0);
+/// assert_eq!(rgb0, [1, 2, 3, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4.
+pub fn rgb0_to_rgbx_inplace(src: &mut [u8]) {
+    apply_mask_4_wide_inplace!(src, xxx0_to_xxxx_mask!(), XXX0_TO_XXXX_OR);
+}
+
+/// Convert RGB0 data to RGBX and store the result to `dst`.
+///
+/// ```rust
+/// use image_swizzle::rgb0_to_rgbx;
+/// let rgb0 = [1, 2, 3, 0];
+/// let mut rgbx = [0; 4];
+/// rgb0_to_rgbx(&rgb0, &mut rgbx);
+/// assert_eq!(rgbx, [1, 2, 3, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4 or if `dst.len` is not equal to `src.len`.
+pub fn rgb0_to_rgbx(src: &[u8], dst: &mut [u8]) {
+    apply_mask_4_wide!(src, dst, xxx0_to_xxxx_mask!(), XXX0_TO_XXXX_OR);
+}
+
+/// Convert BGR0 data to BGRX while overwriting the old BGR0 data in `src`.
+///
+/// ```rust
+/// use image_swizzle::bgr0_to_bgrx_inplace;
+/// let mut bgr0 = [3, 2, 1, 0];
+/// bgr0_to_bgrx_inplace(&mut bgr0);
+/// assert_eq!(bgr0, [3, 2, 1, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4.
+pub fn bgr0_to_bgrx_inplace(src: &mut [u8]) {
+    apply_mask_4_wide_inplace!(src, xxx0_to_xxxx_mask!(), XXX0_TO_XXXX_OR);
+}
+
+/// Convert BGR0 data to BGRX and store the result to `dst`.
+///
+/// ```rust
+/// use image_swizzle::bgr0_to_bgrx;
+/// let bgr0 = [3, 2, 1, 0];
+/// let mut bgrx = [0; 4];
+/// bgr0_to_bgrx(&bgr0, &mut bgrx);
+/// assert_eq!(bgrx, [3, 2, 1, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4 or if `dst.len` is not equal to `src.len`.
+pub fn bgr0_to_bgrx(src: &[u8], dst: &mut [u8]) {
+    apply_mask_4_wide!(src, dst, xxx0_to_xxxx_mask!(), XXX0_TO_XXXX_OR);
+}
+
+/// Convert RGB0 data to BGRX while overwriting the old RGB0 data in `src`.
+///
+/// ```rust
+/// use image_swizzle::rgb0_to_bgrx_inplace;
+/// let mut rgb0 = [1, 2, 3, 0];
+/// rgb0_to_bgrx_inplace(&mut rgb0);
+/// assert_eq!(rgb0, [3, 2, 1, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4.
+pub fn rgb0_to_bgrx_inplace(src: &mut [u8]) {
+    // TODO: Do these in a single call
+    rgb0_to_rgbx_inplace(src);
+    rgba_to_bgra_inplace(src);
+}
+
+/// Convert RGB0 data to BGRX and store the result to `dst`.
+///
+/// ```rust
+/// use image_swizzle::rgb0_to_bgrx;
+/// let rgb0 = [1, 2, 3, 0];
+/// let mut bgrx = [0; 4];
+/// rgb0_to_bgrx(&rgb0, &mut bgrx);
+/// assert_eq!(bgrx, [3, 2, 1, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4 or if `dst.len` is not equal to `src.len`.
+pub fn rgb0_to_bgrx(src: &[u8], dst: &mut [u8]) {
+    apply_x_mask_and_swizzle_4_wide!(src, dst, XXX0_TO_XXXX_OR, RGBA_TO_BGRA_SWIZZLE_IDXS);
+}
+
+/// Convert BGR0 data to RGBX while overwriting the old BGR0 data in `src`.
+///
+/// ```rust
+/// use image_swizzle::bgr0_to_rgbx_inplace;
+/// let mut bgr0 = [3, 2, 1, 0];
+/// bgr0_to_rgbx_inplace(&mut bgr0);
+/// assert_eq!(bgr0, [1, 2, 3, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4.
+pub fn bgr0_to_rgbx_inplace(src: &mut [u8]) {
+    // TODO: Do these in a single call
+    bgr0_to_bgrx_inplace(src);
+    bgra_to_rgba_inplace(src);
+}
+
+/// Convert BGR0 data to RGBX and store the result to `dst`.
+///
+/// ```rust
+/// use image_swizzle::bgr0_to_rgbx;
+/// let bgr0 = [3, 2, 1, 0];
+/// let mut bgrx = [0; 4];
+/// bgr0_to_rgbx(&bgr0 , &mut bgrx);
+/// assert_eq!(bgrx, [1, 2, 3, 255]);
+/// ```
+///
+/// Panics if `src.len` is not multiple of a 4 or if `dst.len` is not equal to `src.len`.
+pub fn bgr0_to_rgbx(src: &[u8], dst: &mut [u8]) {
+    apply_x_mask_and_swizzle_4_wide!(src, dst, XXX0_TO_XXXX_OR, BGRA_TO_RGBA_SWIZZLE_IDXS);
+}
+
+// TODO:
+// pub fn rgb_to_rgbx_inplace(src: &mut [u8]) {}
+// pub fn rgb_to_rgbx(src: &mut [u8], dst: &mut [u8]) {}
+// pub fn rgb_to_bgrx_inplace(src: &mut [u8]) {}
+// pub fn rgb_to_bgrx(src: &mut [u8], dst: &mut [u8]) {}
+// pub fn bgr_to_rgbx_inplace(src: &mut [u8]) {}
+// pub fn bgr_to_rgbx(src: &mut [u8], dst: &mut [u8]) {}
+// pub fn bgr_to_bgrx_inplace(src: &mut [u8]) {}
+// pub fn bgr_to_bgrx(src: &mut [u8], dst: &mut [u8]) {}
 
 #[cfg(test)]
 mod tests {
@@ -177,11 +464,11 @@ mod tests {
     }
 
     #[test]
-    fn test_rgba_to_bgra_inline() {
+    fn test_rgba_to_bgra_inplace() {
         let (width, height) = (1920, 1080);
         let mut rgba_img = generate_xxxx_image(width, height, 111, 222, 100, 255);
         let correct_bgra = generate_xxxx_image(width, height, 100, 222, 111, 255);
-        rgba_to_bgra_inline(&mut rgba_img);
+        rgba_to_bgra_inplace(&mut rgba_img);
         assert_eq!(rgba_img, correct_bgra);
     }
 
@@ -196,11 +483,11 @@ mod tests {
     }
 
     #[test]
-    fn test_bgra_to_rgba_inline() {
+    fn test_bgra_to_rgba_inplace() {
         let (width, height) = (1920, 1080);
         let mut bgra = generate_xxxx_image(width, height, 111, 222, 100, 255);
         let correct_rgba = generate_xxxx_image(width, height, 100, 222, 111, 255);
-        bgra_to_rgba_inline(&mut bgra);
+        bgra_to_rgba_inplace(&mut bgra);
         assert_eq!(bgra, correct_rgba);
     }
 
@@ -215,11 +502,11 @@ mod tests {
     }
 
     #[test]
-    fn test_serial_rgba_to_bgra_inline() {
+    fn test_serial_rgba_to_bgra_inplace() {
         let (width, height) = (1920, 1080);
         let mut rgba_img = generate_xxxx_image(width, height, 111, 222, 100, 255);
         let correct_bgra = generate_xxxx_image(width, height, 100, 222, 111, 255);
-        serial_swizzle_4_wide_inline(&mut rgba_img, &[2, 1, 0, 3]);
+        serial_swizzle_4_wide_inplace(&mut rgba_img, &[2, 1, 0, 3]);
         assert_eq!(rgba_img, correct_bgra);
     }
 
@@ -232,6 +519,84 @@ mod tests {
         serial_swizzle_4_wide(&rgba_img, &mut bgra, &[2, 1, 0, 3]);
         assert_eq!(bgra, correct_bgra);
     }
+
+    #[test]
+    fn test_rgb0_to_rgbx_inplace() {
+        let (width, height) = (1920, 1080);
+        let mut rgb0_img = generate_xxxx_image(width, height, 111, 222, 100, 0);
+        let correct_rgbx = generate_xxxx_image(width, height, 111, 222, 100, 255);
+        rgb0_to_rgbx_inplace(&mut rgb0_img);
+        assert_eq!(rgb0_img, correct_rgbx);
+    }
+
+    #[test]
+    fn test_rgb0_to_rgbx() {
+        let (width, height) = (1920, 1080);
+        let rgb0_img = generate_xxxx_image(width, height, 111, 222, 100, 0);
+        let correct_rgbx = generate_xxxx_image(width, height, 111, 222, 100, 255);
+        let mut rgbx = vec![0; width * height * 4];
+        rgb0_to_rgbx(&rgb0_img, &mut rgbx);
+        assert_eq!(rgbx, correct_rgbx);
+    }
+
+    #[test]
+    fn test_bgr0_to_bgrx_inplace() {
+        let (width, height) = (10, 10);
+        let mut bgr0_img = generate_xxxx_image(width, height, 111, 222, 100, 0);
+        let correct_bgrx = generate_xxxx_image(width, height, 111, 222, 100, 255);
+        bgr0_to_bgrx_inplace(&mut bgr0_img);
+        assert_eq!(bgr0_img, correct_bgrx);
+    }
+
+    #[test]
+    fn test_bgr0_to_bgrx() {
+        let (width, height) = (10, 10);
+        let bgr0_img = generate_xxxx_image(width, height, 111, 222, 100, 0);
+        let correct_bgrx = generate_xxxx_image(width, height, 111, 222, 100, 255);
+        let mut bgrx = vec![0; width * height * 4];
+        bgr0_to_bgrx(&bgr0_img, &mut bgrx);
+        assert_eq!(bgrx, correct_bgrx);
+    }
+
+    #[test]
+    fn test_rgb0_to_bgrx_inplace() {
+        let (width, height) = (1920, 1080);
+        let mut rgb0_img = generate_xxxx_image(width, height, 111, 222, 100, 0);
+        let correct_bgrx = generate_xxxx_image(width, height, 100, 222, 111, 255);
+        rgb0_to_bgrx_inplace(&mut rgb0_img);
+        assert_eq!(rgb0_img, correct_bgrx);
+    }
+
+    #[test]
+    fn test_rgb0_to_bgrx() {
+        let (width, height) = (1920, 1080);
+        let rgb0_img = generate_xxxx_image(width, height, 111, 222, 100, 0);
+        let correct_bgrx = generate_xxxx_image(width, height, 100, 222, 111, 255);
+        let mut bgrx = vec![0; width * height * 4];
+        rgb0_to_bgrx(&rgb0_img, &mut bgrx);
+        assert_eq!(bgrx, correct_bgrx);
+    }
+
+    #[test]
+    fn test_bgr0_to_rgbx_inplace() {
+        let (width, height) = (4, 4);
+        let mut bgr0_img = generate_xxxx_image(width, height, 111, 222, 100, 0);
+        let correct_rgbx = generate_xxxx_image(width, height, 100, 222, 111, 255);
+        bgr0_to_rgbx_inplace(&mut bgr0_img);
+        assert_eq!(bgr0_img, correct_rgbx);
+    }
+
+    #[test]
+    fn test_bgr0_to_rgbx() {
+        let (width, height) = (1920, 1080);
+        let bgr0_img = generate_xxxx_image(width, height, 111, 222, 100, 0);
+        let correct_rgbx = generate_xxxx_image(width, height, 100, 222, 111, 255);
+        let mut rgbx = vec![0; width * height * 4];
+        bgr0_to_rgbx(&bgr0_img, &mut rgbx);
+        assert_eq!(rgbx, correct_rgbx);
+    }
+
+    // Benchmarks
 
     #[bench]
     fn bench_serial_rgba_to_bgra(b: &mut Bencher) {
